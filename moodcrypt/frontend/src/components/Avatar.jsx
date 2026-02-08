0c01180eaf7328c504416bc09caf4035d51cd141
diixitt <dixitmannna@gmail.com>
2026-02-08 06:21:43 +0000

    Delete moodcrypt directory

--- moodcrypt/frontend/src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Avatar({ size = 40 }) {
-  return (
-    <div
-      className="rounded-full bg-gradient-to-br from-gray-600 to-gray-800 border border-gray-700 shrink-0"
-      style={{ width: size, height: size }}
-      aria-label="Anonymous avatar"
-    />
-  )
-}
--- moodcrypt/frontend/src/pages/Following.jsx
+++
@@ -1,7 +0,0 @@
-import HomeFeed from './HomeFeed'
-
-export default function Following() {
-  return (
-    <HomeFeed onlyFollowing />
-  )
-}
